75d7c091f5d925d8557df8cc9ffd264d4bf6ff15
nilaysondagar <nilay@sondagar.com>
2018-09-15 18:32:18 -0700

    Design / Bug Fixing Update
    GENERAL:
    - Fixed second slide-in bug where if you opened the Android OS keyboard, all the elements would resize incorrectly
    - Allowed cards to resize when the width of the page is too small for the cards to slide out to the right (change all units to vh/vw)
    - Added header comments
    
    TO-DO:
    - Fix background scaling
    - Test on different displays and browsers
    
    TIME TAKEN: ~2 HOURS
    TOTAL TIME TAKEN: 8.5 HOURS

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-var inOrOut = 1;
+/*
+ * Filename: script.js
+ * Author: Nilay Sondagar
+ * Date Created: September 11, 2018
+ * Purpose: To allow for the form card to slide in and out on button click for
+ * 			the Duck Tracker website.
+ */
+
+var inOrOut = 1; // tracks whether the slide page is hidden or not
+
+// When the page has loaded, set a fixed viewport width and height, 
+	// to prevent the Android OS keyboard from resizing elements
+document.addEventListener('DOMContentLoaded', function(){
+    let viewheight = $(window).height();
+    let viewwidth = $(window).width();
+    let viewport = document.querySelector("meta[name=viewport]");
+    viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
+});
 
 function showForm() {
 
-	if(window.innerWidth < 1020) {
+	// If the page is smaller than 1020px, slide the form page DOWN, else slide
+		// to the right
+	if(window.matchMedia("(orientation: portrait)").matches) {
 
 		if(inOrOut == 1) {
-			document.getElementById("duckform").style.marginTop = "155%";
+			document.getElementById("duckform").style.marginTop = "70vh";
 			inOrOut = 0;
 		} else if(inOrOut == 0) {	
-			document.getElementById("duckform").style.marginTop = "15%";
-			document.getElementById("duckform").style.marginLeft = "20%";
+			document.getElementById("duckform").style.marginTop = "2.3vh";
 			inOrOut = 1;
 		} // if else
 
 	} else {
 
 		if(inOrOut == 1) {
-			document.getElementById("duckform").style.marginLeft = "123%";
+			document.getElementById("duckform").style.marginLeft = "54vh";
 			inOrOut = 0;
 		} else if(inOrOut == 0) {	
-			document.getElementById("duckform").style.marginTop = "15%";
-			document.getElementById("duckform").style.marginLeft = "20%";
+			document.getElementById("duckform").style.marginLeft = "8.2vh";
 			inOrOut = 1;
 		} // if else
 
-	}
+	} // if else
+
 }// showForm
